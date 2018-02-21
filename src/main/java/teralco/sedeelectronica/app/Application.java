ff258d82f4a1685432291353f6b1cc827aae2242
jsober <jsober@devcenter.es>
2018-02-21 13:11:04 +0100

    feature-captcha-contacto-fragments-forms
    Creado el captcha para el contacto (a falta de añadir la key secret
    personal de quien lo vaya a utilizar)
    Convertidos los formularios con fragmentos para agilizar cambios.

--- src/main/java/teralco/sedeelectronica/app/Application.java
+++ src/main/java/teralco/sedeelectronica/app/Application.java
@@ -3,9 +3,11 @@ package teralco.sedeelectronica.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "teralco.sedeelectronica")
@@ -16,5 +18,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
--- src/main/java/teralco/sedeelectronica/controller/ContactoController.java
+++ src/main/java/teralco/sedeelectronica/controller/ContactoController.java
@@ -1,7 +1,9 @@
 package teralco.sedeelectronica.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.stereotype.Controller;
@@ -10,12 +12,17 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import teralco.sedeelectronica.model.Contacto;
 import teralco.sedeelectronica.service.EmailService;
+import teralco.sedeelectronica.service.RecaptchaService;
 
 @Controller
 public class ContactoController {
+
+	@Autowired
+	private RecaptchaService captchaService;
 
 	private EmailService emailService;
 
@@ -31,9 +38,39 @@ public class ContactoController {
 		return "contacto/formContacto";
 	}
 
-	@PostMapping(value = "/contacto/send")
-	public String save(@Valid @ModelAttribute("contacto") Contacto contacto, BindingResult bindingResult, Model model) {
+	/*
+	 * @PostMapping(value = "/contacto/send")
+	 * 
+	 * @ResponseBody public String save(@Valid @ModelAttribute("contacto") Contacto
+	 * contacto, BindingResult bindingResult, Model model, HttpServletRequest
+	 * request) {
+	 * 
+	 * String response = request.getParameter("g-recaptcha-response");
+	 * captchaService.processResponse(response);
+	 * 
+	 * if (bindingResult.hasErrors()) { return "contacto/formContacto"; }
+	 * 
+	 * try { emailService.sendEmail(contacto); } catch (MailException |
+	 * InterruptedException e) { model.addAttribute("message",
+	 * "Ha ocurrido un error enviando el correo"); return "contacto/formContacto"; }
+	 * 
+	 * return "/contacto/formContacto"; }
+	 */
+
+	@PostMapping("/contacto/send")
+	public String signup(@Valid @ModelAttribute("contacto") Contacto contacto, BindingResult bindingResult, Model model,
+			@RequestParam(name = "g-recaptcha-response") String recaptchaResponse, HttpServletRequest request) {
+
 		if (bindingResult.hasErrors()) {
+			return "contacto/formContacto";
+		}
+
+		String ip = request.getRemoteAddr();
+
+		String captchaVerifyMessage = captchaService.verifyRecaptcha(ip, recaptchaResponse);
+
+		if (StringUtils.isNotEmpty(captchaVerifyMessage)) {
+			model.addAttribute("messageWarning", captchaVerifyMessage);
 			return "contacto/formContacto";
 		}
 
@@ -46,4 +83,5 @@ public class ContactoController {
 
 		return "/contacto/formContacto";
 	}
+
 }
