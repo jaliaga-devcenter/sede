6b412479d1ea0790f89934a8f6df5bace13ee9a3
jaliaga <jaliaga@devcenter.es>
2018-02-23 09:57:44 +0100

    Servicio por categoria
    Implementacion basica de servicios por categoria y filtrado.
    Falta completar el conversor

--- src/main/java/teralco/sedeelectronica/gexflow/client/GexflowClient.java
+++ src/main/java/teralco/sedeelectronica/gexflow/client/GexflowClient.java
@@ -9,15 +9,23 @@ import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 import gexflow.wsdl.ConsultaCatalogoCategorias;
 import gexflow.wsdl.ConsultaCatalogoCategoriasResponse;
+import gexflow.wsdl.ConsultaCatalogoServicio;
+import gexflow.wsdl.ConsultaCatalogoServicioPorCategoria;
+import gexflow.wsdl.ConsultaCatalogoServicioPorCategoriaResponse;
+import gexflow.wsdl.ConsultaCatalogoServicioResponse;
 import gexflow.wsdl.ConsultaIconoCategoria;
 import gexflow.wsdl.ConsultaIconoCategoriaResponse;
+import gexflow.wsdl.EntradaConsultaCategoria;
+import gexflow.wsdl.EntradaConsultaServicioCatalogo;
 import gexflow.wsdl.EstadoRespuesta;
 import gexflow.wsdl.ObjectFactory;
 import gexflow.wsdl.RespuestaCategoriaWS;
 import teralco.sedeelectronica.gexflow.converter.CategoriaConverter;
 import teralco.sedeelectronica.gexflow.converter.IconoConverter;
+import teralco.sedeelectronica.gexflow.converter.ServicioConverter;
 import teralco.sedeelectronica.gexflow.dto.CategoriaDTO;
 import teralco.sedeelectronica.gexflow.dto.IconoDTO;
+import teralco.sedeelectronica.gexflow.dto.ServicioDTO;
 import teralco.sedeelectronica.gexflow.exception.GexflowWSException;
 
 public class GexflowClient extends WebServiceGatewaySupport {
@@ -30,12 +38,16 @@ public class GexflowClient extends WebServiceGatewaySupport {
 	@Autowired
 	private IconoConverter iconoConverter;
 
+	@Autowired
+	private ServicioConverter servicioConverter;
+
 	public List<CategoriaDTO> getCategorias(Integer entidad, String idioma) throws GexflowWSException {
 
 		ConsultaCatalogoCategorias request = new ConsultaCatalogoCategorias();
 		request.setCodigoIdioma(idioma);
 		request.setIdEntidad(entidad);
 
+		@SuppressWarnings("unchecked")
 		JAXBElement<ConsultaCatalogoCategoriasResponse> response = (JAXBElement<ConsultaCatalogoCategoriasResponse>) invokeWS(
 				this.factory.createConsultaCatalogoCategorias(request));
 
@@ -51,6 +63,7 @@ public class GexflowClient extends WebServiceGatewaySupport {
 		request.setIdEntidad(entidad);
 		request.setIdCategoria(idCategoria);
 
+		@SuppressWarnings("unchecked")
 		JAXBElement<ConsultaIconoCategoriaResponse> response = (JAXBElement<ConsultaIconoCategoriaResponse>) invokeWS(
 				this.factory.createConsultaIconoCategoria(request));
 
@@ -61,13 +74,52 @@ public class GexflowClient extends WebServiceGatewaySupport {
 				.setIdCategoria(idCategoria);
 	}
 
+	public List<ServicioDTO> getServicios(Integer entidad, String idioma, Integer idCategoria)
+			throws GexflowWSException {
+
+		ConsultaCatalogoServicioPorCategoria request = new ConsultaCatalogoServicioPorCategoria();
+		request.setIdEntidad(entidad);
+		request.setCodigoIdioma(idioma);
+		request.setEntradaConsultaCategoria(new EntradaConsultaCategoria());
+		request.getEntradaConsultaCategoria().setIdCategoria(idCategoria);
+
+		@SuppressWarnings("unchecked")
+		JAXBElement<ConsultaCatalogoServicioPorCategoriaResponse> response = (JAXBElement<ConsultaCatalogoServicioPorCategoriaResponse>) invokeWS(
+				this.factory.createConsultaCatalogoServicioPorCategoria(request));
+
+		EstadoRespuesta estado = response.getValue().getResultado().getEstadoRespuesta();
+		comprobarError(estado);
+		return this.servicioConverter
+				.createFromEntities(response.getValue().getResultado().getServicios().getServicio());
+	}
+
+	public List<ServicioDTO> buscarServicios(Integer entidad, String idioma, String textoBusqueda)
+			throws GexflowWSException {
+
+		ConsultaCatalogoServicio request = new ConsultaCatalogoServicio();
+		request.setIdEntidad(entidad);
+		request.setCodigoIdioma(idioma);
+		request.setEntradaConsultaServicioCatalogo(new EntradaConsultaServicioCatalogo());
+		request.getEntradaConsultaServicioCatalogo().setDenominacionServicio(textoBusqueda);
+		request.getEntradaConsultaServicioCatalogo().setDescripcion(textoBusqueda);
+
+		@SuppressWarnings("unchecked")
+		JAXBElement<ConsultaCatalogoServicioResponse> response = (JAXBElement<ConsultaCatalogoServicioResponse>) invokeWS(
+				this.factory.createConsultaCatalogoServicio(request));
+
+		EstadoRespuesta estado = response.getValue().getResultado().getEstadoRespuesta();
+		comprobarError(estado);
+		return this.servicioConverter
+				.createFromEntities(response.getValue().getResultado().getServicios().getServicio());
+	}
+
 	private JAXBElement<?> invokeWS(JAXBElement<?> requestWS) {
 		return (JAXBElement<?>) getWebServiceTemplate()
 				.marshalSendAndReceive("http://demo.gexflow.com:8080/gexflowzk/ws/CatalogoTramites", requestWS, null);
 
 	}
 
-	private void comprobarError(EstadoRespuesta estadoRespuesta) throws GexflowWSException {
+	private static void comprobarError(EstadoRespuesta estadoRespuesta) throws GexflowWSException {
 		if (estadoRespuesta.isError()) {
 			throw new GexflowWSException(estadoRespuesta.getMensaje());
 		}
---
+++ src/main/java/teralco/sedeelectronica/gexflow/dto/ServicioDTO.java
@@ -0,0 +1,8 @@
+package teralco.sedeelectronica.gexflow.dto;
+
+public class ServicioDTO extends AbstractDTO {
+
+	public ServicioDTO() {
+
+	}
+}
