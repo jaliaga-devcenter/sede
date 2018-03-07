7c72fd876d477ca770b904b9d77c9c6a5089cd59
jsober <jsober@devcenterord021>
2018-03-07 08:21:15 +0100

    feature-service-error

--- src/test/java/teralco/sedeelectronica/app/TestApplication.java
+++ src/test/java/teralco/sedeelectronica/app/TestApplication.java
@@ -1,14 +1,18 @@
 package teralco.sedeelectronica.app;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@ComponentScan(basePackages = {"teralco.sedeelectronica.gexflow", "teralco.sedeelectronica.verifirma"})
+@EntityScan("teralco.sedeelectronica.model")
+@EnableJpaRepositories("teralco.sedeelectronica.repository")
+@ComponentScan(basePackages = { "teralco.sedeelectronica.gexflow", "teralco.sedeelectronica.verifirma" })
 public class TestApplication {
 
 	public static void main(String[] args) {
-		
+
 	}
 
 }
