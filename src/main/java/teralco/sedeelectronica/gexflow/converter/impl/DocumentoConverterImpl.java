60f429ee1c92dbf3228660cabd9113eb3876e43a
jaliaga <jaliaga@devcenterord016>
2018-03-07 14:26:56 +0100

    Descarga de documentacion asociada

--- src/main/java/teralco/sedeelectronica/gexflow/converter/impl/DocumentoConverterImpl.java
+++ src/main/java/teralco/sedeelectronica/gexflow/converter/impl/DocumentoConverterImpl.java
@@ -1,9 +1,12 @@
 package teralco.sedeelectronica.gexflow.converter.impl;
 
+import org.springframework.stereotype.Component;
+
 import gexflow.wsdl.DocumentoWSJB;
 import teralco.sedeelectronica.gexflow.converter.DocumentoConverter;
 import teralco.sedeelectronica.gexflow.dto.DocumentoDTO;
 
+@Component
 public class DocumentoConverterImpl implements DocumentoConverter {
 
 	@Override
--- src/main/java/teralco/sedeelectronica/gexflow/converter/impl/ServicioConverterImpl.java
+++ src/main/java/teralco/sedeelectronica/gexflow/converter/impl/ServicioConverterImpl.java
@@ -1,13 +1,18 @@
 package teralco.sedeelectronica.gexflow.converter.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import gexflow.wsdl.ServicioWSJB;
+import teralco.sedeelectronica.gexflow.converter.DocumentoConverter;
 import teralco.sedeelectronica.gexflow.converter.ServicioConverter;
 import teralco.sedeelectronica.gexflow.dto.ServicioDTO;
 
 @Component
 public class ServicioConverterImpl implements ServicioConverter {
+
+	@Autowired
+	private DocumentoConverter documentoConverter;
 
 	@Override
 	public ServicioDTO createFrom(ServicioWSJB entity) {
@@ -19,6 +24,9 @@ public class ServicioConverterImpl implements ServicioConverter {
 				entity.getFormaInicio(), entity.getCosteFormaPago(), entity.getPlazoMaximoResolucion(),
 				entity.getOrganosCompetentes(), entity.getRecursos(), entity.getFundamentoLegal(),
 				entity.getPreguntasFrecuentes(), entity.getContactar());
+
+		servicio.getDocumentos().addAll(
+				this.documentoConverter.createFromEntities(entity.getDocumentacionRelacionada().getDocumento()));
 		return servicio;
 	}
 
