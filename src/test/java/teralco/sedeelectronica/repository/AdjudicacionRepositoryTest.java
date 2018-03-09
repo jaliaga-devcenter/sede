5d0d062b60afb1043ed1a755446bdcf251a8acba
jsober <jsober@devcenterord021>
2018-03-09 14:39:32 +0100

    feature-normativa-sede
    Fixing Repository Test

--- src/test/java/teralco/sedeelectronica/repository/AdjudicacionRepositoryTest.java
+++ src/test/java/teralco/sedeelectronica/repository/AdjudicacionRepositoryTest.java
@@ -2,32 +2,39 @@ package teralco.sedeelectronica.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import teralco.sedeelectronica.app.TestApplication;
 import teralco.sedeelectronica.model.Adjudicacion;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { TestApplication.class })
+@DataJpaTest
+@Transactional
 public class AdjudicacionRepositoryTest {
+	@Autowired
+	private TestEntityManager entityManager;
 
 	@Autowired
 	private AdjudicacionRepository adjudicacionRepository;
 
 	@Test
-	@Ignore
 	public void saveTest() {
 		// ARRANGE
 		Adjudicacion adju = new Adjudicacion();
 		adju.setDenominacion("una denominacion");
+		this.entityManager.persist(adju);
+		this.entityManager.flush();
 
 		// ACT
-		Adjudicacion found = this.adjudicacionRepository.save(adju);
+		Adjudicacion found = this.adjudicacionRepository.findById((long) 1);
 
 		// ASSERT
 		assertThat(found.getDenominacion()).isEqualTo(adju.getDenominacion());
