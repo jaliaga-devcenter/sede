e7bf48e75f4834baddc41f1cce1541551eaa20ea
jsober <jsober@devcenterord021>
2018-04-11 12:59:13 +0200

    feature

--- src/test/java/tecalco/sedeelectronica/admin/controller/AdminNormativaControllerTest.java
+++ src/test/java/tecalco/sedeelectronica/admin/controller/AdminNormativaControllerTest.java
@@ -5,13 +5,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,13 +21,13 @@ import teralco.sedeelectronica.app.TestApplication;
 @SpringBootTest(classes = TestApplication.class)
 @AutoConfigureMockMvc
 @WithMockUser(roles = { "ADMIN_SEDE" })
+@ActiveProfiles("test")
 public class AdminNormativaControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
 
 	@Test
-	@Ignore
 	public void getAperturas() throws Exception {
 		this.mvc.perform(get("/admin/normativa")).andExpect(status().isOk());
 	}
---
+++ src/test/java/teralco/sedeelectornica/gexflow/dto/DocumentoDTOTest.java
@@ -0,0 +1,29 @@
+package teralco.sedeelectornica.gexflow.dto;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import teralco.sedeelectronica.gexflow.dto.DocumentoDTO;
+
+public class DocumentoDTOTest {
+
+	@Test
+	public void testDocumentoDTO() {
+		Integer id = 14;
+		String pNombre = "nom";
+		String pDesc = "desc";
+		String pEnlace = "www.google.es";
+
+		// ARRANGE
+		DocumentoDTO doc = new DocumentoDTO(id, pNombre, pDesc, pEnlace);
+		// ASSERT
+
+		assertEquals(id, doc.getIdDocumento());
+		assertEquals(pDesc, doc.getTipoDocumento());
+		assertEquals(pNombre, doc.getNombre());
+		assertEquals(pEnlace, doc.getEnlace());
+
+	}
+
+}
