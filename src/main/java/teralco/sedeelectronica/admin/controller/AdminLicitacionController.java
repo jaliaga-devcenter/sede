71155dcd0e8f959a70af1a06b536cb4a18c9bf71
jaliaga <jaliaga@devcenterord016>
2018-05-02 12:31:17 +0200

    Pending changes

--- src/main/java/teralco/sedeelectronica/admin/controller/AdminLicitacionController.java
+++ src/main/java/teralco/sedeelectronica/admin/controller/AdminLicitacionController.java
@@ -35,8 +35,6 @@ import teralco.sedeelectronica.utils.PageWrapper;
 @Controller
 public class AdminLicitacionController {
 
-	private static final String MEDIOMODEL = "medios";
-
 	private static String list = "admin/licitaciones/licitaciones";
 	private static String redirList = "redirect:/admin/licitaciones";
 	private static String form = "admin/licitaciones/formLicitacion";
