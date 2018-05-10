59fbc02f4d40dab91d6e015c26cec04057a9dbea
jaliaga <jaliaga@devcenterord016>
2018-05-10 10:13:40 +0200

    Fix multilanguage

--- src/main/java/teralco/sedeelectronica/model/Parada.java
+++ src/main/java/teralco/sedeelectronica/model/Parada.java
@@ -2,19 +2,24 @@ package teralco.sedeelectronica.model;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
+import javax.persistence.PostLoad;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
+
+import teralco.sedeelectronica.utils.LanguageUtils;
 
 @Entity
 public class Parada extends BaseModel {
@@ -27,6 +32,8 @@ public class Parada extends BaseModel {
 	@ElementCollection(fetch = FetchType.LAZY)
 	@CollectionTable(name = "parada_lenguaje", joinColumns = @JoinColumn(name = "parada_id"))
 	private List<ParadaLenguaje> traducciones = new ArrayList<>();
+
+	private transient Map<String, ParadaLenguaje> traduccionesMap = new HashMap<>();
 
 	@Temporal(TemporalType.DATE)
 	@NotNull
@@ -47,6 +54,17 @@ public class Parada extends BaseModel {
 
 	public void setTraducciones(List<ParadaLenguaje> pTraducciones) {
 		this.traducciones = pTraducciones;
+
+	}
+
+	public String getDescripcion() {
+		String language = LanguageUtils.getLanguage();
+		return this.traduccionesMap.get(language).getDescripcion();
+	}
+
+	@PostLoad
+	public void loadTranslationsMap() {
+		this.traducciones.forEach(traduccion -> this.traduccionesMap.put(traduccion.getIdioma(), traduccion));
 	}
 
 }
